c1fe2bec44136f035849c143e9b40336dde80447
YinFeng <jddj123@163.com>
2017-01-24 16:19:58 +0800

    Update base.js

--- base.js
+++ base.js
@@ -1,16 +1,3 @@
-(function(window) {
-    var theUA = window.navigator.userAgent.toLowerCase();
-    if ((theUA.match(/msie\s\d+/) && theUA.match(/msie\s\d+/)[0]) || (theUA.match(/trident\s?\d+/) && theUA.match(/trident\s?\d+/)[0])) {
-        var ieVersion = theUA.match(/msie\s\d+/)[0].match(/\d+/)[0] || theUA.match(/trident\s?\d+/)[0];
-        if (ieVersion < 9) {
-            var str = "你的浏览器版本太low了\n已经和时代脱轨了 :(";
-            var str2 = "推荐使用:<a href='https://www.baidu.com/s?ie=UTF-8&wd=%E8%B0%B7%E6%AD%8C%E6%B5%8F%E8%A7%88%E5%99%A8' target='_blank' style='color:#cc0'>谷歌</a>," + "<a href='https://www.baidu.com/s?ie=UTF-8&wd=%E7%81%AB%E7%8B%90%E6%B5%8F%E8%A7%88%E5%99%A8' target='_blank' style='color:#cc0'>火狐</a>," + "<a href='https://www.baidu.com/s?ie=UTF-8&wd=%E7%8C%8E%E8%B1%B9%E6%B5%8F%E8%A7%88%E5%99%A8' target='_blank' style='color:#cc0'>猎豹</a>,其他双核急速模式";
-            document.writeln("<pre style='text-align:center;color:#fff;margin:0;background-color:#0cc; height:100%;border:0;position:fixed;top:0;left:0;width:100%;z-index:1234'>" +
-                "<h2 style='padding-top:200px;margin:0'><strong>" + str + "<br/></strong></h2><p>" +
-                str2 + "</p><h2 style='margin:0'><strong> 如果你的使用的是双核浏览器,请切换到极速模式访问<br/></strong></h2></pre>"); 
-        }
-    } 
-})(window);
 $(function() {
     var $input=$("input");
     $("main").mousedown(function(event) { 
